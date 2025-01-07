6bedc33379cd35ebb2aeb7e9d1ec74cd0052d943
kathytran88 <kathyngananhtran@gmail.com>
2025-01-06 21:56:20 -0500

    a

--- app.py
+++ app.py
@@ -216,4 +216,5 @@ def analysis():
     return render_template('analysis.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
